b9e45c9e6476fc5048e7c47d2643b81a415d0259
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-03-02 18:06:59 +0530

    useing useNavigate instead of Link

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../css/dashboard.css";
 
 const Dashboard = () => {
@@ -8,57 +8,64 @@ const Dashboard = () => {
   const jwtToken = localStorage.getItem("token");
   const userId = localStorage.getItem("userId");
   const role = localStorage.getItem("role");
+  const navigate = useNavigate();
+  const [validOrNot, setValidOrNot] = useState(false);
 
-    const server__uri = import.meta.env.VITE_SERVER_COMMON_URI;
-
+  const server__uri = import.meta.env.VITE_SERVER_COMMON_URI;
 
   useEffect(() => {
     const fetchData = async () => {
-      try{
-      if (role === "ADMIN") {
-        const response = await axios.get(
-          `${server__uri}/members`,
-          {
+      try {
+        if (role === "ADMIN") {
+          const response = await axios.get(`${server__uri}/members`, {
             headers: {
               Authorization: `Bearer ${jwtToken}`,
             },
-          },
-        );
-        setData(response.data);
-      } else {
-        const response = await axios.get(
-          `${server__uri}/members/${userId}`,
-          {
+          });
+          setData(response.data);
+          setValidOrNot(true);
+        } else {
+          const response = await axios.get(`${server__uri}/members/${userId}`, {
             headers: {
               Authorization: `Bearer ${jwtToken}`,
             },
-          },
-        );
-        setData([response.data]);
-      }
-      }catch(error){
+          });
+          setData([response.data]);
+          setValidOrNot(true);
+        }
+      } catch (error) {
         alert(error);
+        setValidOrNot(false);
+        navigate("/");
       }
-      
     };
     fetchData();
   }, []);
 
+  const logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
   return (
     <div>
-      <h2>Dashboard</h2>
-      <div className="card-container">
-        {data.map((user) => (
-          <div key={user.id} className="cards">
-            <p>Name: {user.name}</p>
-            <p>Email: {user.email}</p>
-            <p>PhoneNo. : {user.phoneNumber}</p>
+      {validOrNot && (
+        <div>
+          <h2>Dashboard</h2>
+          <div className="card-container">
+            {data.map((user) => (
+              <div key={user.id} className="cards">
+                <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+                <p>PhoneNo. : {user.phoneNumber}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <p>
-        Want to LogOut? <Link to="/">LogOut here</Link>
-      </p>
+          <p>
+            Want to LogOut? <button onClick={logout}>LogOut here</button>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
